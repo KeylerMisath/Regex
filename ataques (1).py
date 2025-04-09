960dfb808dc4d75c257b43becd90573874e689c5
KeylerMisath <keylermisathm07@gmail.com>
2025-04-08 19:19:18 -0500

    ataques (1).py

--- ataques (1).py
+++ ataques (1).py
@@ -12,24 +12,20 @@ def extractFromRegularExpression(regex, data):
 
 regex = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?\[(\d{2}\/[a-zA-Z]{3}\/\d{4})(\:\d{2}\:\d{2}\:\d{2}).*?(\"\b[A-Z]{1,7}\s\S+\s\S+)\s(\d{3})"
 
-# Leer y combinar todos los logs
 data = ""
 for i in range(7):
     ruta = fr"C:\Users\carol\Downloads\SotM34-anton\SotM34\http\access_log{'' if i == 0 else '.' + str(i)}"
     with open(ruta, "r") as file:
         data += file.read()
 
-# Extraer datos
 resultado = extractFromRegularExpression(regex, data)
 
-# Guardar IPs únicas y otros datos
 if resultado:
     for ip, fecha, hora, url, codigo in resultado:
         ipsAlmacenadas.add(ip)
         fechasAlmacenadas.add(fecha)
         codigos_errorAlmacenados.add(codigo)
 
-# Llamar a la API por cada IP única
 datosGeolocalizados = []
 for ip in ipsAlmacenadas:
     try:
@@ -49,9 +45,7 @@ for ip in ipsAlmacenadas:
         }
     datosGeolocalizados.append(geoInfo)
 
-# Mostrar en consola en formato bonito
 print(json.dumps(datosGeolocalizados, indent=4, ensure_ascii=False))
 
-# (Opcional) Guardar resultado en archivo
 with open("ips_geolocalizadas.json", "w", encoding="utf-8") as f:
     json.dump(datosGeolocalizados, f, indent=4, ensure_ascii=False)
